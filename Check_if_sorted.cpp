ccc9bc3afcc8587f5ea6560804e90e559a2eabe0
Aryan Tiwari <146500011+aryan19022004@users.noreply.github.com>
2025-06-22 19:02:46 +0530

    Update Check_if_sorted.cpp

--- Check_if_sorted.cpp
+++ Check_if_sorted.cpp
@@ -5,13 +5,11 @@ using namespace std;
 int main()
 {
     int arr[5] = {1,8,3,4,5};
+    // Time complexity will be O(n)
     bool sorted = true;
     for(int i =1; i<5; i++)
     {
-        if(arr[i]>= arr[i-1])
-        {
-
-        }
+        if(arr[i]>= arr[i-1]){}
         else
         {
              sorted = false;
