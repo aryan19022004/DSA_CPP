85e65577ff213975c658835c648c7a9820ac8fba
Aryan Tiwari <146500011+aryan19022004@users.noreply.github.com>
2025-06-22 18:25:39 +0530

    Update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main()
 {
     int arr[5] = {5,4,6,2,7};
-    
-    
+    //BruteForce solution 
+    /*the brute force solution with be like choosing the last element after sorting the arry in ascending order*/
+
+    //Optimal solution
     int largest = arr[0];
     for(int i =1;i<5;i++){
         if(arr[i]>largest){
