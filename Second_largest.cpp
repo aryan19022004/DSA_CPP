c6210f44dbd7ccc7be677439ecb6057bb052798d
Aryan Tiwari <146500011+aryan19022004@users.noreply.github.com>
2025-06-22 18:31:11 +0530

    Update Second_largest.cpp

--- Second_largest.cpp
+++ Second_largest.cpp
@@ -4,7 +4,31 @@ using namespace std;
 int main()
 {
     int arr[9] = {5,4,6,2,7,999,99,10000,9999};
+    //BruteForce solution
+    /* 
+    1. choosing the 2nd last element after sorting an array
+    2. here the time complexity of sorting will be O(n2) 
+    */
 
+    //Better solution
+    int largest = arr[0];
+    for(int i =1;i<9;i++){
+        if(arr[i]>largest){
+            largest = arr[i];
+        }
+    }
+    int second_largest = -1;
+    for(int i= 0;i<9;i++){
+        if(arr[i]>second_largest && arr[i]<largest){
+            second_largest = arr[i];
+        }
+    }
+    //O(n) + 0(n) --> it will be 0(2n) here in better solution we are traversing the array twice 
+
+    //Optimal solution
+    /*
+      Here we are traversing the array single time so here time complexity will be O(n)
+    */
     int largest = INT_MIN+1;
     int second_largest = INT_MIN;
     for(int i =0;i<9;i++){
