d41a0a0f187be314f96b34fed6d1f168da096055
Aryan Tiwari <146500011+aryan19022004@users.noreply.github.com>
2025-07-21 12:24:08 +0530

    Create MajorityElement.cpp

---
+++ MajorityElement.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Program to find Mjority Element Of The array
+
+  /*  
+  Time complexity: O(n)
+  Space complexity: O(n)
+  
+  
+    vector<int> arr = {1,2,2,3,3,3,4,4,4,4,4,4,4,4,7};
+    map<int,int>mp;
+    int n = arr.size();
+    for(int i =0;i<n;i++){
+        mp[arr[i]]++ ;
+    }
+
+   for(auto i:mp){
+    if(i.second >n/2){
+        cout<<"The majority element of the array is :"<<i.first;
+        return 0;
+    }
+   }
+   cout<<"There is no Such majority element";
+   */
+
+    return 0;
+}
