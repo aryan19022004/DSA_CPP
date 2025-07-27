23954f0084ab02824549a849dd4bb254245e56a5
Aryan Tiwari <146500011+aryan19022004@users.noreply.github.com>
2025-07-27 09:44:34 +0530

    Update MultipleMissingNumber.cpp

--- MultipleMissingNumber.cpp
+++ MultipleMissingNumber.cpp
@@ -1,24 +1,20 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
-#include<bits/stdc++.h>
+#include <vector>
 using namespace std;
+
 int main() {
-   vector<int> arr = {1,2,3,4,5,6,10};
-   int n = arr.size();
-   int diff = 0;
-   int num = 0;
-   for(int i = 0;i<n-1;i++){
-       if(arr[i+1] - arr[i] != 1){
-            diff = arr[i+1] - arr[i];
-            num = arr[i];
-           break;
-       }
-   }
-   
-    cout<<"Missing NUMBERS ARE : ";
-   for(int i = 1;i<diff;i++){
-       cout<<num +i<<" "; ;
-   }
-  
+    vector<int> arr = {1, 2, 4, 5, 7, 8, 10};
+    int n = arr.size();
+
+    cout << "Missing numbers are: ";
+    for (int i = 1; i < n; i++) {
+        int diff = arr[i] - arr[i - 1];
+        if (diff > 1) {
+            for (int j = 1; j < diff; j++) {
+                cout << arr[i - 1] + j << " ";
+            }
+        }
+    }
+
     return 0;
 }
