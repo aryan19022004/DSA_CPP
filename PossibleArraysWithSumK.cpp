d1579471b09a1d45e31754774576f86200c3d179
Aryan Tiwari <146500011+aryan19022004@users.noreply.github.com>
2025-08-14 12:56:00 +0530

    Update PossibleArraysWithSumK.cpp

--- PossibleArraysWithSumK.cpp
+++ PossibleArraysWithSumK.cpp
@@ -25,3 +25,44 @@ int main() {
 
     return 0;
 }  
+
+
+
+/*
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr = {10, 22, 12, 3, 0, 6};
+    int n = arr.size();
+    int left = 0;
+    int sum = 0;
+    int k = 37;
+
+    for(int right = 0; right < n; right++){
+        sum += arr[right];
+
+        while(left <= right && sum > k){
+            sum -= arr[left];
+            left++;
+        }
+
+        if(sum == k){
+            for(int i = left; i <= right; i++){
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+
+
+            sum -= arr[left];
+            left++;
+        }
+    }
+
+    return 0;
+}
+
+
+*/
